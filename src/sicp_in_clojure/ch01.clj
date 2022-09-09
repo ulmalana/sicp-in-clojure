b49ce0dcda80387b770e1a0e961f8575d0f576ca
rz <muhamaul@gmail.com>
2022-09-09 11:26:10 +0700

    [SICP Clojure] finished chapter 1

--- src/sicp_in_clojure/ch01.clj
+++ src/sicp_in_clojure/ch01.clj
@@ -479,3 +479,102 @@ circumference
     (+ (* x (square a))
        (* y b)
        (* a b))))
+
+(defn close-enough?
+  [x y]
+  (< (abs (- x y)) 0.001))
+
+(defn search
+  [f neg-point pos-point]
+  (let [midpoint (average neg-point pos-point)]
+    (if (close-enough? neg-point pos-point)
+      midpoint
+      (let [test-value (f midpoint)]
+        (cond
+          (pos? test-value) (search f neg-point midpoint)
+          (neg? test-value) (search f midpoint pos-point)
+          :else midpoint)))))
+
+(defn half-interval-method
+  [f a b]
+  (let [a-value (f a)
+        b-value (f b)]
+    (cond
+      (and (neg? a-value) (pos? b-value)) (search f a b)
+      (and (neg? b-value) (pos? a-value)) (search f b a)
+      :else (throw (Exception. (str "Values are not of opposite sign: " a b))))))
+
+(defn sin [n]
+  (Math/sin n))
+
+(defn cos [n]
+  (Math/cos n))
+
+(half-interval-method sin 2.0 4.0)
+;; => 3.14111328125 
+
+(half-interval-method (fn [x]
+                        (- (* x x x) (* 2 x) 3))
+                      1.0
+                      2.0)
+;; => 1.89306640625
+
+(def tolerance 0.00001)
+
+(defn fixed-point
+  [f first-guess]
+  (letfn [(close-enough? [v1 v2]
+            (< (abs (- v1 v2)) tolerance))
+          (try-it [guess]
+               (let [next (f guess)]
+                 (if (close-enough? guess next)
+                   next
+                   (try-it next))))]
+    (try-it first-guess)))
+
+(fixed-point cos 1.0)
+;; => 0.7390822985224024
+
+(fixed-point (fn [y] (+ (sin y) (cos y))) 1.0)
+;; => 1.2587315962971173
+
+(defn average-damp
+  [f]
+  (fn [x] (average x (f x))))
+
+((average-damp square) 10)
+;; => 55
+
+(defn sqrt'
+  [x]
+  (fixed-point (average-damp (fn [y] (/ x y))) 1.0))
+
+(defn cube-root
+  [x]
+  (fixed-point (average-damp (fn [y] (/ x (square y)))) 1.0))
+
+(def dx 0.00001)
+
+(defn deriv
+  [g]
+  (fn [x] (/ (- (g (+ x dx)) (g x)) dx)))
+
+((deriv cube) 5) 
+;; => 75.00014999664018
+
+(defn newton-transform
+  [g]
+  (fn [x] (- x (/ (g x) ((deriv g) x)))))
+
+(defn newtons-method
+  [g guess]
+  (fixed-point (newton-transform g) guess))
+
+(defn sqrt''
+  [x]
+  (newtons-method
+   (fn [y] (- (square y) x)) 1.0))
+
+(defn fixed-point-of-transform
+  [g transform guess]
+  (fixed-point (transform g) guess))
